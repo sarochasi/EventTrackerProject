08c3e710f753debd157e47f493af80d5833aff83
Sarocha <rocha.right007@gmail.com>
2024-09-06 16:02:55 -0600

    Stub Spring Boot REST project, add JPA project dependency

---
+++ TrackMyJob/src/test/java/com/skilldistillery/jobapplications/TrackMyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jobapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
