7c1fa783e0a17dc05a482c3c83155179bdc90445
Sarocha <rocha.right007@gmail.com>
2024-09-07 16:42:47 -0600

    remove unused imports

--- TrackMyJob/src/main/java/com/skilldistillery/jobapplications/controllers/UserController.java
+++ TrackMyJob/src/main/java/com/skilldistillery/jobapplications/controllers/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skilldistillery.jobapplications.entities.Job;
 import com.skilldistillery.jobapplications.entities.User;
 import com.skilldistillery.jobapplications.services.UserService;
 
